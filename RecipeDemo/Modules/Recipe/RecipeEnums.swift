dae169494007829d9b2abbee29b50a437091ad10
Ali Tarek <ali.tareq@hmaserv.com>
2021-06-14 21:31:03 +0200

    add Recipe Enums

---
+++ RecipeDemo/Modules/Recipe/RecipeEnums.swift
@@ -0,0 +1,46 @@
+//
+//  RecipeEnums.swift
+//  RecipeDemo
+//
+//  Created by Ali Tarek on 6/14/21.
+//
+
+import Foundation
+
+enum SearchError: Error
+{
+    case emptySearch
+    case invalidSearchKeyowrd
+    case emptySearchSuggestion
+}
+
+extension SearchError: LocalizedError
+{
+    var errorDescription: String?
+    {
+        switch self
+        {
+        case .emptySearch:
+            return NSLocalizedString("Empty Search Results, Try Again!", comment: "")
+            
+        case .invalidSearchKeyowrd:
+            return NSLocalizedString("Plese Enter Valid Search Keyword", comment: "")
+            
+        case .emptySearchSuggestion:
+            return NSLocalizedString("empty search suggestion", comment: "")
+        }
+    }
+}
+
+enum HealthFilter: String
+{
+    case all
+    case lowSugar = "low-sugar"
+    case keto = "keto-friendly"
+    case vegan = "vegan"
+}
+
+enum RecipeSegues: String
+{
+    case recipeDetails = "showRecipeDetails"
+}
